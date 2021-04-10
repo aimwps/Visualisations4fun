23ced9d9a834128e6e064cc8d3d894c13c1e44db
aimwps <mwpskinner@gmail.com>
2021-04-10 13:53:55 +0100

    worked on adding genres by year

--- imdb_plots.py
+++ imdb_plots.py
@@ -52,35 +52,40 @@ def released_by_year(df):
 def genre_by_year(df):
     imdb = df[['release_year','genre']]
     imdb["bracket"] = pd.cut(imdb.release_year, [i for i in range(1920, 2025,5)])
-    #print(imdb.head())
-    gb_bracket = imdb.groupby(["bracket", "genre"]).release_year.count().reset_index()
-    year_slices = gb_bracket['bracket'].unique()
-    print(year_slices)
-    print(type(year_slices))
-    graph_data = []
-    for year in year_slices:
-        graph_data.append((str(year), gb_bracket[gb_bracket.bracket == year]))
+    gb = imdb.groupby(["bracket", "genre"]).count().reset_index()
+    ch = imdb['bracket'].astype(str)
 
-    # last_year = graph_data[-1]
+    imdb.plot(type="scatter")
+
+
+
+    # gb['bracket'] = ch
+    # gb.columns = ["bracket", "genre", "count"]
     # plt.figure(figsize=(10,10))
-    # plt.scatter(x=)
-    # transform_brackets_to_strings = group_brackets["bracket"].astype(str)
-    # group_brackets['bracket']= transform_brackets_to_strings
+    # plt.plot(gb['bracket'], gb['count'])
+    # plt.show()
+    # # for bracket in gb['bracket'].unique():
+    #     plt.plot()
+    # print(gb.info())
 
 
-    #print(type(x.iloc[0]))
 
-    #check = imdb[imdb['bracket'] == '(1920, 1925]']
-    #print(check)
-    # check = imdb[imdb['genre']=='Western']
-    # check2 = check[check['release_year'] > 2015]
-    # print(len(check2))
 
-    #gb = imdb.groupby([ "brackets"]).genre.count().reset_index()
-    #print(gb.head(100))
-# click_source_by_month_pivot = click_source_by_month.pivot(columns='month',
-# index='utm_source',
-# values='id').reset_index()
+    # #print(imdb.head())
+    # #gb_bracket = imdb.groupby(["bracket", "genre"]).count().reset_index()
+    # # year_slices = gb_bracket['bracket'].unique()
+    #
+    # plt.figure(figsize=(10,10))
+    # genres = imdb['genre'].unique()
+    # print(genres)
+    # plt.plot()
+    #
+    # graph_data = []
+    # for year in year_slices:
+    #     graph_data.append((str(year), gb_bracket[gb_bracket.bracket == year]))
+    #
+
+
 
 
 genre_by_year(imdb)
